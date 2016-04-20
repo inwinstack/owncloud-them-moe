7e60e9a861a4209fd9ef1518d81dd4b9b6f9dfd3
JamieHo3312 <jamie.h@inwinstack.com>
2016-04-20 14:50:56 +0800

    Update the l10n

---
+++ apps/files_sharing/l10n/zh_TW.js
@@ -0,0 +1,8 @@
+OC.L10N.register(
+    "files_sharing",
+    {
+    "Shared with you" : "別人分享給我",
+    "Shared with others" : "我分享給別人",
+    "Shared by link" : "使用連結分享"
+},
+"nplurals=1; plural=0;");
--- settings/l10n/zh_TW.js
+++ settings/l10n/zh_TW.js
@@ -1,6 +1,6 @@
 OC.L10N.register(
     "settings",
     {
-    "Show First Run Wizard again" : "再次顯示服務使用規範"
+    "Show First Run Wizard again" : "顯示服務使用規範"
 },
 "nplurals=1; plural=0;");
