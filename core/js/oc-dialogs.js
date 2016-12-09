da9cc9937df3d8e8a51ec5b0333aab734624e406
SimonLin <simon.l@inwinstack.com>
2016-12-09 11:43:59 +0800

    [bugfix] remove hour+1

--- core/js/oc-dialogs.js
+++ core/js/oc-dialogs.js
@@ -759,7 +759,7 @@ var OCdialogs = {
       var year = ''+date.getFullYear();
       var month = (date.getMonth()+1) < 10 ? '0'+(date.getMonth()+1) : ''+(date.getMonth()+1);
       var day = date.getDate() <10 ? '0' + date.getDate() : ''+date.getDate();
-      var hour = (date.getHours()+1) < 10 ? '0'+(date.getHours()+1) : ''+(date.getHours()+1);
+      var hour = date.getHours() < 10 ? '0'+date.getHours() : ''+date.getHours();
       var minute = date.getMinutes() < 10 ? '0'+date.getMinutes() : ''+date.getMinutes();
       var sec = date.getSeconds() < 10 ? '0'+date.getSeconds() : ''+date.getSeconds();
       
