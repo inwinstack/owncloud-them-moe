3da4bd3c16eac403a75c9efbc2f236ab27a6ff73
ChienChih <chienchih.c@inwinstack.com>
2017-03-13 17:50:50 +0800

    open a new window while entry settings except logout in displayname

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -81,7 +81,7 @@
 				<ul>
 				<?php foreach($_['settingsnavigation'] as $entry):?>
 					<li>
-						<a href="<?php print_unescaped($entry['href']); ?>"
+						<a target="_blank" href="<?php print_unescaped($entry['href']); ?>"
 							<?php if( $entry["active"] ): ?> class="active"<?php endif; ?>>
 							<img class="svg" alt="" src="<?php print_unescaped($entry['icon']); ?>">
 							<?php p($entry['name']) ?>
