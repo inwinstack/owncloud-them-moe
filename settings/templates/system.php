ff23cf69f0fa4ec2504cf7864b2a4874fb4c81c4
JamieHo3312 <jamie.h@inwinstack.com>
2017-07-11 15:51:55 +0800

    [Feature]Redesign system information page for download application

--- settings/templates/system.php
+++ settings/templates/system.php
@@ -7,33 +7,53 @@ $clients = array(
 );
 ?>
 <div id="app-content">
-    <div id="downloads" style="float: left;text-align: center;">
-    	<img style="float: left;" src="<?php print_unescaped(image_path('settings', 'app_download.png')); ?>" alt="" width="50%" height="50%">
-    	<span style= "display:block"><?php p($l->t('Use the cloud storage service anytime, anywhere')); ?></span>
-        <span><?php p($l->t('These files will be automatically synced to your other devices')); ?></span>
-    	<div class="section">
-    		<span style= "display:block">PC版</span>
-            <a href="https://storage.edu.tw/教育部雲端儲存桌面代理應用程式.exe" class="client">
-                <span class="client-info"><img src="<?php print_unescaped(image_path('settings', 'apps.svg')); ?>">Windows</span>
-                <span class="client-info">7, 8.x, 10</span>
-            </a>
-            <a href="https://storage.edu.tw/教育部雲端儲存桌面代理應用程式.pkg" class="client">
-                <span class="client-info"><img src="<?php print_unescaped(image_path('settings', 'apps.svg')); ?>">MAC OSX</span>
-                <span class="client-info">10.7+</span>
-            </a>
-            <a href="https://storage.edu.tw/Ubuntu_14.04-MOE-Storage-Cloud-Install.sh" class="client">
-                <span class="client-info"><img src="<?php print_unescaped(image_path('settings', 'apps.svg')); ?>">Ubuntu</span>
-                <span class="client-info">14.04</span>
-            </a>
+    <div id="downloads" class="sections clearfix clientsbox center">
+        <div class="client-left">
+            <img src="<?php print_unescaped(image_path('settings', 'app_download.png')); ?>" alt="">
         </div>
-        <div class="section">
-            <span style= "display:block">手機與平板</span>
-            <a href="<?php p($clients['ios']); ?>" target="_blank">
-            	<img width="10%" src="<?php print_unescaped(image_path('settings', 'ios.png')); ?>">
-            </a>
-             <a href="<?php p($clients['android']); ?>" target="_blank">
-            	<img width="10%" src="<?php print_unescaped(image_path('settings', 'google.png')); ?>">
-            </a>
+        
+        <div class="client-right">
+            <div class="margin-bottom">
+                <span class="client-title"><?php p($l->t('Use the cloud storage service anytime, anywhere')); ?></span>
+                <span><?php p($l->t('These files will be automatically synced to your other devices')); ?></span>
+            </div>
+        
+            <div class="margin-bottom">
+                <span class="client-text">PC版</span>
+                <a href="https://storage.edu.tw/教育部雲端儲存桌面代理應用程式.exe" class="client">
+                    <img class="client-img" src="<?php print_unescaped(image_path('settings', 'windows-8.png')); ?>">
+                    <span class="">Windows</span>
+                    <span class="client-info">7, 8.x, 10</span>
+                </a>
+                <a href="https://storage.edu.tw/教育部雲端儲存桌面代理應用程式.pkg" class="client">
+                    <img class="client-img" src="<?php print_unescaped(image_path('settings', 'apple-big-logo.png')); ?>">
+                    <span class="">MAC OSX</span>
+                    <span class="client-info">10.7+</span>
+                </a>
+                <a href="https://storage.edu.tw/Ubuntu_14.04-MOE-Storage-Cloud-Install.sh" class="client">
+                    <img class="client-img" src="<?php print_unescaped(image_path('settings', 'Ubuntu.png')); ?>">
+                    <span class="">Ubuntu</span>
+                    <span class="client-info">14.04</span>
+                </a>
+            </div>
+            
+            <div class="margin-bottom">
+                <span class="client-text">手機與平板</span>
+                <div style="display: inline-block;">
+                    <a style="display: block;" href="<?php p($clients['ios']); ?>" target="_blank">
+                        <img class="client-store" src="<?php print_unescaped(image_path('settings', 'ios.png')); ?>">
+                    </a>
+                    
+                    <img class="client-store" src="<?php print_unescaped(image_path('settings', 'iOS App Download.jpg')); ?>">
+                </div>
+                
+                <div style="display: inline-block;">
+                    <a style="display: block;" href="<?php p($clients['android']); ?>" target="_blank">
+                        <img class="client-store" src="<?php print_unescaped(image_path('settings', 'google.png')); ?>">
+                    </a>
+                    <img class="client-store" src="<?php print_unescaped(image_path('settings', 'Android App Download.jpg')); ?>">
+                </div>
+            </div>
         </div>
     </div>
 </div>
